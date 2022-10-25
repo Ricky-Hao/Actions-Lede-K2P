f169fcc67a36fc250696d0a6b8f91d3a12d995e7
Ricky-Hao <14084342+Ricky-Hao@users.noreply.github.com>
2022-10-25 14:33:35 +0800

    Add K2P config

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,8 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add AX6 support
+git config user.name test
+git config user.email test@test.com
 git remote add mi-ipq807x https://github.com/lovehackintosh/lede
 git fetch mi-ipq807x
 git cherry-pick e6d2a2641145b30f4c602c6d996a7b1d0fcff770
